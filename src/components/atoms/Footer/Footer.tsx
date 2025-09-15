8cf1809a2806ff96fd9cd34e85f85a717a51ca7c
PopelinovDanilo <popelinov.danilo@gmail.com>
2025-09-15 18:46:37 +0300

    feat: adding basic Footer

---
+++ src/components/atoms/Footer/Footer.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+
+export const Footer: React.FC = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__container}>
+        <div className={styles.footer__logo}>
+          <img
+            src="src\assets\icons\brand\logo.svg"
+            className={styles.footer__img}
+          />
+        </div>
+
+        <nav className={styles.footer__nav}>
+          {/* чи можемо ми використовувати className="uppercase-text" так? */}
+          <a
+            className="uppercase-text"
+            href="https://github.com/fs-jun25-team-4-tech-check/nice-gadgets"
+          >
+            Github
+          </a>
+          <a
+            className="uppercase-text"
+            href="/contacts"
+          >
+            Contacts
+          </a>
+          <a
+            className="uppercase-text"
+            href="/rights"
+          >
+            Rights
+          </a>
+        </nav>
+        <div className={styles.footer__backToTop}>
+          <span className="small">Back to top</span>
+          <button onClick={scrollToTop}>
+            <img src="src\assets\icons\navigation\chevron-arrow-up.svg"></img>
+          </button>
+        </div>
+      </div>
+    </footer>
+  );
+};
