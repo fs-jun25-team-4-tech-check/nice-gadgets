f3792e0727437dc3771500fb75df80b2e1dd2f6b
Roman Danuliyk <romandanuliyk@gmail.com>
2025-09-15 19:28:22 +0300

    feat(TTC-20): added backButton atom

--- src/components/atoms/Buttons/Back/BackButton.tsx
+++ src/components/atoms/Buttons/Back/BackButton.tsx
@@ -1,8 +1,18 @@
 /* eslint-disable react/prop-types */
+import { PiCaretLeft as LeftArrow } from 'react-icons/pi';
+import styles from './BackButton.module.scss';
 type Props = {
   children: React.ReactNode;
 };
 
 export const BackButton: React.FC<Props> = ({ children }) => {
-  return <button type="button">{children}</button>;
+  return (
+    <button
+      type="button"
+      className={styles.backButton}
+    >
+      <LeftArrow />
+      {children}
+    </button>
+  );
 };
