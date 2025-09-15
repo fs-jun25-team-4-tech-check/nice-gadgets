c5c6bdb50b9f0d06dbb88372e04b7b0ba6588b00
Oleksandr Vannovskyi <alexandervannovskiy@gmail.com>
2025-09-15 11:31:52 +0300

    fix: config for global styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use '@/styles/_variables.scss' as *; @use '@/styles/_mixins.scss' as *;`,
+      },
+    },
+  },
 });
