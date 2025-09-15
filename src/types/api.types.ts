458084236256508926f24cd65ba208d3a901a717
Petro Bahrynovskyi <pbahrynovskyi@gmail.com>
2025-09-15 17:24:00 +0300

    feat(TTC-24): add fetch functions and typization (#9)

---
+++ src/types/api.types.ts
@@ -0,0 +1,90 @@
+export interface Product {
+  id: number;
+  category: string;
+  itemId: string;
+  name: string;
+  fullPrice: number;
+  price: number;
+  screen: string;
+  capacity: string;
+  color: string;
+  ram: string;
+  year: number;
+  image: string;
+}
+
+export interface PhoneDescription {
+  title: string;
+  text: string[];
+}
+
+export interface Phone {
+  id: string;
+  namespaceId: string;
+  name: string;
+  capacityAvailable: string[];
+  capacity: string;
+  priceRegular: number;
+  priceDiscount: number;
+  colorsAvailable: string[];
+  color: string;
+  images: string[];
+  description: PhoneDescription[];
+  screen: string;
+  resolution: string;
+  processor: string;
+  ram: string;
+  camera: string;
+  zoom: string;
+  cell: string[];
+}
+
+export interface TabletDescription {
+  title: string;
+  text: string[];
+}
+
+export interface Tablet {
+  id: string;
+  namespaceId: string;
+  name: string;
+  capacityAvailable: string[];
+  capacity: string;
+  priceRegular: number;
+  priceDiscount: number;
+  colorsAvailable: string[];
+  color: string;
+  images: string[];
+  description: TabletDescription[];
+  screen: string;
+  resolution: string;
+  processor: string;
+  ram: string;
+  camera: string;
+  zoom: string;
+  cell: string[];
+}
+
+export interface AccessoryDescription {
+  title: string;
+  text: string[];
+}
+
+export interface Accessory {
+  id: string;
+  namespaceId: string;
+  name: string;
+  capacityAvailable: string[];
+  capacity: string;
+  priceRegular: number;
+  priceDiscount: number;
+  colorsAvailable: string[];
+  color: string;
+  images: string[];
+  description: AccessoryDescription[];
+  screen: string;
+  resolution: string;
+  processor: string;
+  ram: string;
+  cell: string[];
+}
