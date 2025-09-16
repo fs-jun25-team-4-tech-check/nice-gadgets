10401135103642217ab1726fa1eb9b606e56cc7c
Petro Bahrynovskyi <pbahrynovskyi@gmail.com>
2025-09-16 20:34:50 +0300

    feat(TTC-26):create banner  slider at home page

---
+++ src/components/organisms/MainSlider/MainSlider.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import { ActionButton } from '../../atoms';
+
+import 'swiper/swiper.css';
+
+import styles from './MainSlider.module.scss';
+
+export const MainSlider: React.FC = () => {
+  return (
+    <div className={styles.sliderWrapper}>
+      <div className={styles.sliderInnerWrapper}>
+        <ActionButton
+          variant="slider"
+          direction="left"
+          className={styles.prev}
+        />
+        <Swiper
+          modules={[Navigation, Pagination]}
+          loop
+          spaceBetween={10}
+          slidesPerView={1}
+          navigation={{
+            prevEl: `.${styles.prev}`,
+            nextEl: `.${styles.next}`,
+          }}
+          pagination={{
+            el: `.${styles.pagination}`,
+            clickable: true,
+            renderBullet: (index, className) =>
+              `<span class="${className} ${styles.paginationBullet}"></span>`,
+          }}
+          breakpoints={{
+            640: { slidesPerView: 1 },
+            1200: { slidesPerView: 1 },
+          }}
+        >
+          <SwiperSlide>
+            <img
+              src="/img/phones/apple-iphone-14-slider/slider-image-iphone-14.webp"
+              alt="iPhone 14"
+              className={styles.slideImage}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="/img/phones/apple-iphone-14-slider/slider-image-iphone-14.webp"
+              alt="iPhone 14"
+              className={styles.slideImage}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="/img/phones/apple-iphone-14-slider/slider-image-iphone-14.webp"
+              alt="iPhone 14"
+              className={styles.slideImage}
+            />
+          </SwiperSlide>
+        </Swiper>
+
+        <ActionButton
+          variant="slider"
+          direction="right"
+          className={styles.next}
+        />
+      </div>
+      <div className={`${styles.pagination} ${styles.paginationContainer}`} />
+    </div>
+  );
+};
--- src/components/templates/HomeLayout/HomeLayout.tsx
+++ src/components/templates/HomeLayout/HomeLayout.tsx
@@ -1,7 +1,12 @@
 import styles from './HomeLayout.module.scss';
-
+import { MainSlider } from '../../organisms/MainSlider/MainSlider';
 const HomeLayout = () => {
-  return <h1 className={styles.homeTitleExample}>Home Page</h1>;
+  return (
+    <>
+      <h1 className={styles.homeTitleExample}>Home Page</h1>
+      <MainSlider />
+    </>
+  );
 };
 
 export default HomeLayout;
