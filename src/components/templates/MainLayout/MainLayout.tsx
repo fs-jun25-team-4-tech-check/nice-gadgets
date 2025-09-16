710b7f0cd2ef9f48cb2ee196f403fea99654c332
PopelinovDanilo <popelinov.danilo@gmail.com>
2025-09-16 21:34:01 +0300

    feat(TTC7): add footer in main page

--- src/components/templates/MainLayout/MainLayout.tsx
+++ src/components/templates/MainLayout/MainLayout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import styles from './MainLayout.module.scss';
+import { Footer } from '../../organisms/Footer/Footer';
 
 const MainLayout = () => {
   return (
-    <>
+    <div className={styles.wrapper}>
       <main className={styles.container}>
         <Outlet />
       </main>
-    </>
+      <Footer />
+    </div>
   );
 };
 
