55e613c6cec47af036203afbbf65c4da2dfacb65
Oleksandr Vannovskyi <alexandervannovskiy@gmail.com>
2025-09-17 11:23:28 +0300

    fix(TTC-29): images path fix

--- src/data/categoryBannerData.ts
+++ src/data/categoryBannerData.ts
@@ -1,22 +1,25 @@
 import type { CategoryBannerData } from '../types/Category';
+import phonesImg from '../assets/images/categories/category-phones.png';
+import tabletsImg from '../assets/images/categories/category-tablets.png';
+import accessoriesImg from '../assets/images/categories/category-accessories.png';
 
 export const staticCategoryData: CategoryBannerData[] = [
   {
     categorySlug: 'phones',
     name: 'Mobile Phones',
-    imgLink: 'src/assets/images/categories/category-phones.png',
+    imgLink: phonesImg,
     backgroundColor: '#c69d76',
   },
   {
     categorySlug: 'tablets',
     name: 'Tablets',
-    imgLink: 'src/assets/images/categories/category-tablets.png',
+    imgLink: tabletsImg,
     backgroundColor: '#848caf',
   },
   {
     categorySlug: 'accessories',
     name: 'Accessories',
-    imgLink: 'src/assets/images/categories/category-accessories.png',
+    imgLink: accessoriesImg,
     backgroundColor: '#ce9999',
   },
 ];
