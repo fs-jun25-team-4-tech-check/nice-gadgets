3cf5015ce64c019a635e7b06e66a4465f7f99b11
Oleksandr Vannovskyi <alexandervannovskiy@gmail.com>
2025-09-17 15:39:59 +0300

    adding header to mainlayout

--- src/components/templates/MainLayout/MainLayout.tsx
+++ src/components/templates/MainLayout/MainLayout.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import styles from './MainLayout.module.scss';
+import Header from '../../organisms/Header/Header';
 
 const MainLayout = () => {
   return (
     <>
+      <Header />
+
       <main className={styles.container}>
         <Outlet />
       </main>
