8f3ab454cdf31596b017a7601dc0cafba636e225
Роман Данилюк <159816709+romkas228@users.noreply.github.com>
2025-09-17 11:05:10 +0300

    TTC-29_create-shop-by-category (#20)
    * feat(TTC-29): added Category type
    
    * feat(TTC-29): implemented shopByCategory
    
    * feat(TTC-29): wrapped category images with links
    
    * refactor(TTC-29): finished task
    
    ---------
    
    Co-authored-by: Oleksandr Vannovskyi <alexandervannovskiy@gmail.com>

--- src/components/templates/HomeLayout/HomeLayout.tsx
+++ src/components/templates/HomeLayout/HomeLayout.tsx
@@ -1,7 +1,21 @@
+import type React from 'react';
+import type { CategoryBanner } from '../../../types/Category';
+import { ShopByCategory } from '../../organisms/ShopByCategory';
 import styles from './HomeLayout.module.scss';
 
-const HomeLayout = () => {
-  return <h1 className={styles.homeTitleExample}>Home Page</h1>;
+type Props = {
+  shopByCategoryBanners: CategoryBanner[];
+};
+
+const HomeLayout: React.FC<Props> = ({ shopByCategoryBanners }) => {
+  return (
+    <>
+      <h1 className={styles.homeTitleExample}>Home Page</h1>
+
+      <h2>Shop by category</h2>
+      <ShopByCategory categories={shopByCategoryBanners} />
+    </>
+  );
 };
 
 export default HomeLayout;
---
+++ src/data/categoryBannerData.ts
@@ -0,0 +1,22 @@
+import type { CategoryBannerData } from '../types/Category';
+
+export const staticCategoryData: CategoryBannerData[] = [
+  {
+    categorySlug: 'phones',
+    name: 'Mobile Phones',
+    imgLink: 'src/assets/images/categories/category-phones.png',
+    backgroundColor: '#c69d76',
+  },
+  {
+    categorySlug: 'tablets',
+    name: 'Tablets',
+    imgLink: 'src/assets/images/categories/category-tablets.png',
+    backgroundColor: '#848caf',
+  },
+  {
+    categorySlug: 'accessories',
+    name: 'Accessories',
+    imgLink: 'src/assets/images/categories/category-accessories.png',
+    backgroundColor: '#ce9999',
+  },
+];
