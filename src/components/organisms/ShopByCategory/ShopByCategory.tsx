1a1a12291be3752e36acbf020c53d7cbff84f84b
Роман Данилюк <159816709+romkas228@users.noreply.github.com>
2025-09-18 17:45:43 +0300

    TTC-57 use api to fill shop by category with data (#27)
    * feat(TTC-57): added useProductCountByCategory hook to hooks
    
    * feat(TTC-57): Used useProductCountsByCategory hook to fill shopByCategory with count data
    
    * feat(TTC-57): added isLoading state to models count in shopByCategory organism
    
    * fix(TTC-57): deleted old useProductCountsByCategory
    
    * feat(TTC-57): refactored HomePage to new structure

--- src/components/organisms/ShopByCategory/ShopByCategory.tsx
+++ src/components/organisms/ShopByCategory/ShopByCategory.tsx
@@ -1,55 +1,23 @@
-import { Link } from 'react-router-dom';
-import styles from './ShopByCategory.module.scss';
 import type { CategoryBanner } from '../../../types/Category';
 import type React from 'react';
+import { useProductCategoryCounts } from '../../../hooks';
+import { staticCategoryData } from '../../../data';
+import { ShopByCategoryUI } from './ShopByCategoryUI';
 
-type Props = {
-  categories: CategoryBanner[];
-};
+export const ShopByCategory: React.FC = () => {
+  const { data: counts, isLoading } = useProductCategoryCounts();
 
-export const ShopByCategory: React.FC<Props> = ({ categories }) => {
-  const baseCategoryUrl = '/catalog';
+  const shopByCategoryBanners: CategoryBanner[] = staticCategoryData.map(
+    (category) => ({
+      ...category,
+      productCount: counts?.[category.categorySlug] || 0,
+    }),
+  );
 
   return (
-    <>
-      <section className={styles.categories}>
-        {categories.map((category) => {
-          return (
-            <div
-              key={category.categorySlug}
-              className={styles.categories_card}
-            >
-              <Link
-                style={{ backgroundColor: category.backgroundColor }}
-                className={styles.categories_imageWrapper}
-                to={`${baseCategoryUrl}/${category.categorySlug}`}
-              >
-                <div
-                  style={{ backgroundColor: category.backgroundColor }}
-                  className={styles.categories_imageWrapper}
-                >
-                  <img
-                    className={styles.categories_image}
-                    src={category.imgLink}
-                    alt={category.name}
-                  />
-                </div>
-              </Link>
-
-              <Link
-                className={styles.categories_link}
-                to={`${baseCategoryUrl}/${category.categorySlug}`}
-              >
-                {category.name}
-              </Link>
-
-              <p className={styles.categories_modelsNumber}>
-                {category.productCount} models
-              </p>
-            </div>
-          );
-        })}
-      </section>
-    </>
+    <ShopByCategoryUI
+      banners={shopByCategoryBanners}
+      isLoading={isLoading}
+    />
   );
 };
