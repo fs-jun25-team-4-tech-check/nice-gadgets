d04d20214361b48668de06355feb36cef79397f1
Yana Ovcharik <yanaovcharik@gmail.com>
2025-09-18 18:43:27 +0300

    TTC-51 add toasts for favourites and add to cart (#25)
    * feat(TTC-51): add toasts for favourites and add to cart
    
    * fix(TTC-51): fix timeots and make toast more flexibility, add border
    
    ---------
    
    Co-authored-by: Oleksandr Vannovskiy <alexandervannovskiy@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ToastProvider from './providers/ToastProvider.tsx';
+import ToastRenderer from './components/atoms/ToastRenderer/ToastRenderer.tsx';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ToastProvider>
+        <App />
+        <ToastRenderer />
+      </ToastProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
