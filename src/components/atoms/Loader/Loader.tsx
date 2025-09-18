1bb81474db4ed13dec86c6b30c816e35fb40db4f
Роман Данилюк <159816709+romkas228@users.noreply.github.com>
2025-09-18 16:15:42 +0300

    feat(TTC-58): Refactored loader and added custom size prop to make Loader more flexible (#29)
    Co-authored-by: Oleksandr Vannovskiy <alexandervannovskiy@gmail.com>

--- src/components/atoms/Loader/Loader.tsx
+++ src/components/atoms/Loader/Loader.tsx
@@ -1,20 +1,20 @@
 import classNames from 'classnames';
 import styles from './Loader.module.scss';
-import { type LoaderSize } from './Loader.types';
 
 type Props = {
-  size?: LoaderSize;
+  size?: number;
   className?: string;
 };
 
-const Loader: React.FC<Props> = ({ size = 'md', className }) => {
-  const loaderClasses = classNames(
-    styles.loader,
-    styles[`loader--${size}`],
-    className,
-  );
+const Loader: React.FC<Props> = ({ size = 50, className }) => {
+  const loaderClasses = classNames(styles.loader, className);
 
-  return <div className={loaderClasses}></div>;
+  return (
+    <div
+      style={{ width: `${size}px`, padding: `${size / 7}px` }}
+      className={loaderClasses}
+    ></div>
+  );
 };
 
 export default Loader;
