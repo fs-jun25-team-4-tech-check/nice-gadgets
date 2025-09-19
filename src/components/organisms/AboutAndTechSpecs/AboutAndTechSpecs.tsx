c49b6db941ec829e3862399677df877b5298ea29
Petro Bahrynovskyi <pbahrynovskyi@gmail.com>
2025-09-19 16:27:01 +0300

    fix(TTC-68):Fixed some scss styles

--- src/components/organisms/AboutAndTechSpecs/AboutAndTechSpecs.tsx
+++ src/components/organisms/AboutAndTechSpecs/AboutAndTechSpecs.tsx
@@ -7,25 +7,28 @@ interface AboutAndTechSpecsProps {
 export const AboutAndTechSpecs: React.FC<AboutAndTechSpecsProps> = ({
   product,
 }) => {
-  switch (product.category) {
-    case 'phones':
-      // TypeScript knows 'product' is of type 'Phone' here
-      return (
-        <div>
-          <p>Camera: {product.camera}</p>
-          <p>Zoom: {product.zoom}</p>
-        </div>
-      );
-    case 'tablets':
-      // TypeScript knows 'product' is of type 'Tablet' here
-      return (
-        <div>
-          <p>Camera: {product.camera}</p>
-          <p>Zoom: {product.zoom}</p>
-        </div>
-      );
-    case 'accessories':
-      // No specific properties to render for Accessory
-      return null;
+  // switch (product.category) {
+  //   case 'phones':
+  //     // TypeScript knows 'product' is of type 'Phone' here
+  //     return (
+  //       <div>
+  //         <p>Camera: {product.camera}</p>
+  //         <p>Zoom: {product.zoom}</p>
+  //       </div>
+  //     );
+  //   case 'tablets':
+  //     // TypeScript knows 'product' is of type 'Tablet' here
+  //     return (
+  //       <div>
+  //         <p>Camera: {product.camera}</p>
+  //         <p>Zoom: {product.zoom}</p>
+  //       </div>
+  //     );
+  //   case 'accessories':
+  //     // No specific properties to render for Accessory
+  //     return null;
+  // }
+  if (product) {
+    return null;
   }
 };
