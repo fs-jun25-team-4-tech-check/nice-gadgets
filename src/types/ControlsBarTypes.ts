15a6294f080a9494729c4d2d48f90f2cc267c8f9
PopelinovDanilo <145030343+PopelinovDanilo@users.noreply.github.com>
2025-09-19 16:27:27 +0300

    TTC-38 implement selects (#31)
    * feat(TTC38): create components
    
    * feat(TTC-38): create style, logic for items on page
    
    * refactor(TTC38): refactor for new DropItem, add ControlsBarTypes
    
    ---------
    
    Co-authored-by: Oleksandr Vannovskiy <alexandervannovskiy@gmail.com>

---
+++ src/types/ControlsBarTypes.ts
@@ -0,0 +1,8 @@
+export type SortOption = 'Newest' | 'Price_ascending' | 'Price_descending';
+
+export type PaginationOption = 12 | 16 | 24 | 36;
+
+export interface DropdownItem {
+  value: string;
+  displayName: string;
+}
