af50763a7ab699f2657420cdc99e0b34cc7463d0
PopelinovDanilo <popelinov.danilo@gmail.com>
2025-09-19 12:59:41 +0300

    refactor(TTC38): refactor for new DropItem, add ControlsBarTypes

--- src/components/molecules/ControlsBar/ControlsBar.tsx
+++ src/components/molecules/ControlsBar/ControlsBar.tsx
@@ -1,36 +1,57 @@
-// ControlsBar.tsx
-
 import React from 'react';
 import { DropdownAtom } from '../../atoms/DropdownAtom/DropdownAtom';
 import styles from './ControlsBar.module.scss';
+import {
+  type SortOption,
+  type PaginationOption,
+  type DropdownItem,
+} from '../../../types/ControlsBarTypes';
 
 interface ControlsBarProps {
-  perPage: number;
-  onPerPageChange: (value: number) => void;
+  sortOption: SortOption;
+  onSortChange: (value: SortOption) => void;
+  perPage: PaginationOption;
+  onPerPageChange: (value: PaginationOption) => void;
 }
 
 export const ControlsBar: React.FC<ControlsBarProps> = ({
+  sortOption,
+  onSortChange,
   perPage,
   onPerPageChange,
 }) => {
+  const sortItems: DropdownItem[] = [
+    { value: 'Newest', displayName: 'Newest' },
+    { value: 'Price_ascending', displayName: 'Price ↑' },
+    { value: 'Price_descending', displayName: 'Price ↓' },
+  ];
+
+  const paginationItems: DropdownItem[] = [
+    { value: '12', displayName: '12' },
+    { value: '16', displayName: '16' },
+    { value: '24', displayName: '24' },
+    { value: '36', displayName: '36' },
+  ];
+
   return (
     <div className={styles.controlsBarContainer}>
       <div className={styles.controlsBar}>
         <DropdownAtom
           label="Sort by"
-          items={['Newest', 'Price ↑', 'Price ↓']}
+          items={sortItems}
           placeholder="Select"
-          onSelect={() => 0}
+          value={sortOption}
+          onSelect={(val) => onSortChange(val as SortOption)}
           variant="sort"
         />
       </div>
       <div className={styles.controlsBar}>
         <DropdownAtom
           label="Items on page"
-          items={['12', '16', '24', '36']}
+          items={paginationItems}
           value={String(perPage)}
           placeholder="Select"
-          onSelect={(val) => onPerPageChange(Number(val))}
+          onSelect={(val) => onPerPageChange(Number(val) as PaginationOption)}
           variant="pagination"
         />
       </div>
