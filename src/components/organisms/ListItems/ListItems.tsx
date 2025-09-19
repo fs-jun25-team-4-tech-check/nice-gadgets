67d714e5c757918e65861f1e7123ffba94301dd0
Roman Danuliyk <romandanuliyk@gmail.com>
2025-09-19 15:49:48 +0300

    feat(TTC-36): updated List styles, added className prop to ProductCard to add custom classes

--- src/components/organisms/ListItems/ListItems.tsx
+++ src/components/organisms/ListItems/ListItems.tsx
@@ -12,16 +12,9 @@ export const ListItems: React.FC<ListItemsProps> = ({ products }) => {
     <div className={styles.listContainer}>
       {products?.map((product) => (
         <ProductCard
-          key={product.id}
-          image={product.image}
-          title={product.name}
-          price={product.price}
-          oldPrice={product.fullPrice}
-          screen={product.screen}
-          capacity={product.capacity}
-          ram={product.ram}
-          isInCart={false}
-          isFavorite={false}
+          key={product.itemId}
+          product={product}
+          className={styles.listItem}
         />
       ))}
     </div>
