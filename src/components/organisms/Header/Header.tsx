773163b8eda2692cf78740a49aadfdfb97036808
Petro Bahrynovskyi <pbahrynovskyi@gmail.com>
2025-09-23 16:21:16 +0300

    feat(TTC-52): created light mode (#53)
    * feat(TTC-52): created light mode
    
    * fix(TTC-52): fixed bug with a button size
    
    ---------
    
    Co-authored-by: Oleksandr Vannovskiy <alexandervannovskiy@gmail.com>

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,16 +1,21 @@
 import styles from './Header.module.scss';
 import { NavLink } from 'react-router-dom';
-import logo from './../../../assets/icons/brand/logo.svg';
+import logo_dark from './../../../assets/icons/brand/logo_dark.svg';
+import logo_light from './../../../assets/icons/brand/logo-light.svg';
 import { HeaderButton } from '../../atoms/Buttons/HeaderButton';
 import { BurgerMenu } from '../../molecules/BurgerMenu';
+import ThemeSwitcher from '../../atoms/ThemeSwitcher/ThemeSwitcher';
 import { useEffect, useState } from 'react';
 import { useCart } from '../../../hooks/useCart';
 import { useFavs } from '../../../hooks/useFavs';
+import { useGlobalStore } from '../../../stores/globalStore';
 
 const Header = () => {
-  const { count } = useCart();
+  const { cart } = useCart();
   const { favs } = useFavs();
   const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
+
+  const theme = useGlobalStore((state) => state.theme);
   useEffect(() => {
     if (isBurgerMenuOpen) {
       document.body.style.overflow = 'hidden';
@@ -37,20 +42,28 @@ const Header = () => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
+  const currentTheme =
+    theme === 'auto' ?
+      window.matchMedia('(prefers-color-scheme: dark)').matches ?
+        'dark'
+      : 'light'
+    : theme;
+
   return (
     <>
       <header className={styles.header}>
+        {' '}
         <NavLink
           onClick={() => setIsBurgerMenuOpen(false)}
           to={'/'}
           className={styles.logo}
         >
+          {' '}
           <img
-            src={logo}
+            src={currentTheme === 'light' ? logo_light : logo_dark}
             alt="Nice Gadgets Logo"
-          />
+          />{' '}
         </NavLink>
-
         <nav>
           <ul className={styles.nav}>
             <li>
@@ -95,8 +108,8 @@ const Header = () => {
             </li>
           </ul>
         </nav>
-
         <div className={styles.icons}>
+          <ThemeSwitcher />
           <HeaderButton
             variant="favourites"
             className={styles.desktopIcons}
@@ -105,7 +118,7 @@ const Header = () => {
           <HeaderButton
             variant="cart"
             className={styles.desktopIcons}
-            notifCount={count}
+            notifCount={cart.length}
           />
           <HeaderButton
             variant={isBurgerMenuOpen ? 'close' : 'burger'}
