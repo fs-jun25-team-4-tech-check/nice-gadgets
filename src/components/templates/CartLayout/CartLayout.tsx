dde7bd227c93c024218fd8724b0ad261b35fbe4e
Oleksandr Vannovskyi <alexandervannovskiy@gmail.com>
2025-09-24 17:11:57 +0300

    refactor(TTC-114): improved loading states and some style fixes

--- src/components/templates/CartLayout/CartLayout.tsx
+++ src/components/templates/CartLayout/CartLayout.tsx
@@ -1,10 +1,8 @@
 import { useCart } from '../../../hooks/useCart';
 import { BackButton } from '../../atoms';
-import Loader from '../../atoms/Loader/Loader';
 import styles from './CartLayout.module.scss';
 
 type Props = {
-  isLoading: boolean;
   cartListSection: React.ReactNode;
   cartSummarySection: React.ReactNode;
 };
@@ -12,25 +10,20 @@ type Props = {
 const CartLayout: React.FC<Props> = ({
   cartListSection,
   cartSummarySection,
-  isLoading,
 }) => {
   const { count } = useCart();
   return (
     <>
       <BackButton className={styles.backButton}>Back</BackButton>
+
       <h1 className={styles.cartTitle}>Cart</h1>
+
       {count === 0 && <p>No items in cart</p>}
-      {isLoading && count > 0 && (
-        <div className={styles.loaderWrapper}>
-          <Loader />
-        </div>
-      )}
-      {!isLoading && count > 0 && (
-        <div className={styles.cartContent}>
-          {cartListSection}
-          {cartSummarySection}
-        </div>
-      )}
+
+      <div className={styles.cartContent}>
+        {cartListSection}
+        {cartSummarySection}
+      </div>
     </>
   );
 };
