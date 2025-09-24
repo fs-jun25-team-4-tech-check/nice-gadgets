c12a7a80c534eac97a30af6ea6a0f00d480cff40
Oleksandr Vannovskiy <alexandervannovskiy@gmail.com>
2025-09-24 15:57:36 +0300

    feat(TTC-113): added loading states to catalogue listitem (#62)

--- src/components/organisms/ListItems/ListItems.tsx
+++ src/components/organisms/ListItems/ListItems.tsx
@@ -2,29 +2,35 @@ import React from 'react';
 import ProductCard from '../../molecules/ProductCard/ProductCard';
 import type { Product } from '../../../types/api.types';
 import styles from './ListItems.module.scss';
-import Loader from '../../atoms/Loader/Loader';
+import ProductCardSkeleton from '../../molecules/ProductCard/ProductCardSkeleton';
 
 interface ListItemsProps {
   products: Product[] | undefined;
-  className?: string;
   isLoading?: boolean;
+  itemsCount?: number;
+  className?: string;
 }
 
 export const ListItems: React.FC<ListItemsProps> = ({
   products,
-  className = '',
   isLoading = false,
+  itemsCount = 12,
+  className = '',
 }) => {
   return (
     <div className={`${styles.listContainer} ${className}`}>
-      {isLoading && <Loader size={52} />}
-      {products?.map((product) => (
-        <ProductCard
-          key={product.itemId}
-          product={product}
-          className={styles.listItem}
-        />
-      ))}
+      {isLoading ?
+        Array.from({ length: itemsCount }).map((_, index) => (
+          <ProductCardSkeleton key={index} />
+        ))
+      : products?.map((product) => (
+          <ProductCard
+            key={product.itemId}
+            product={product}
+            className={styles.listItem}
+          />
+        ))
+      }
     </div>
   );
 };
